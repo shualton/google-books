ee2db414c39d3603e0ef511bda8ed8069f2e9f31
shualton <shualton@gmail.com>
2019-07-30 21:22:55 -0700

    added render function to pages.js

--- client/src/pages/Results/Results.js
+++ client/src/pages/Results/Results.js
@@ -10,7 +10,6 @@ class Results extends Component {
     state = {
         books: [],
         target: "",
-        noResults: false
       };
 
       componentMount() {
@@ -23,8 +22,93 @@ class Results extends Component {
           });
         } else {
           this.setState({
-            noResults: true
           });
         }
+      }
+      saveBook = book => {
+        API.saveBook(book)
+          .then(res => {
+            const currentBooks = this.state.books;
+            const filterBooks = currentBooks.filter(book => book.id !== res.data.id);
+            this.setState({
+              books: filterBooks
+            });
+          })
+          .catch(err => console.log(err));
+      }
+
+      render() {
+        if (this.state.noResults) {
+          return (
+            <div>
+              <Jumbotron>
+                <h1 className="display-4">(React) Google Books Search</h1>
+                <p className="lead">Search for and annotate books of interest.</p>
+                <hr className="my-4" />
+                <p className="lead">
+                  <Link className="btn btn-default btn-lg" to="/" role="button">New Search</Link>
+                  <Link className="btn btn-default btn-lg" to="/saved" role="button">Saved Books</Link>
+                </p>
+              </Jumbotron>
+              <Container>
+                <Link to="/">No results - click here to search again.</Link>
+              </Container>
+            </div>
+          )
+        }
+        return (
+          <div>
+            <Header>
+              <h1 className="display-4">Google Book Searcher</h1>
+              <hr className="my-4" />
+              <p className="lead">
+                <Link className="btn btn-default btn-lg" to="/" role="button">Search</Link>
+                <Link className="btn btn-default btn-lg" to="/saved" role="button">Saved</Link>
+              </p>
+            </Header>
+            <Container>
+              <h2>Search Results</h2>
+              <List>
+                {this.state.books.map((book, index) => (
+                  <Items key={book.id}>
+                    <div className="date-div">
+                      <a
+                        key={"" + index + book.id}
+                        href={book.volumeInfo.infoLink}
+                        target={this.state.target}
+                      >
+                        {book.volumeInfo.title}
+                      </a>
+                        <p>Written By {book.volumeInfo.authors[0]}</p>
+                      <p>
+                      <img align="left" style={{paddingRight:10}}
+                        src={book.volumeInfo.imageLinks.smallThumbnail} alt="new"
+                      />
+                        {book.volumeInfo.description}
+                      </p>
+                    </div>
+                    <div className="book-btn-div">
+                      <Button
+                        key={"" + book.id + index}
+                        btntype="info"
+                        disabled={book.volumeInfo.infoLink === "/"}
+                        onClick={() => this.saveBook({
+                          title: book.volumeInfo.title,
+                          author: book.volumeInfo.authors[0],
+                          description: book.volumeInfo.description,
+                          image: book.volumeInfo.imageLinks.smallThumbnail,
+                          link: book.volumeInfo.infoLink,
+                          _id: book.id
+                        })}
+                      >
+                        Save
+                      </Button>
+                    </div>
+                  </Items>
+                ))}
+              </List>
+            </Container>
+          </div>
+        );
       }
 }
