fb5effb7e7eacc876a62b2a7f63a0060a5231bf2
shualton <shualton@gmail.com>
2019-07-31 14:18:34 -0700

    updated render function in save.js page

--- client/src/pages/Search/Search.js
+++ client/src/pages/Search/Search.js
@@ -32,4 +32,43 @@ class Search extends Component {
               .catch(err => console.log(err));
           }
     };
+
+    render() {
+        if (this.state.toResults) {
+          return <Redirect to={{
+            pathname: "/results",
+            data: { results: this.state.results }
+          }} />
+        }
+        return (
+          <div>
+            <Header>
+              <h1 className="display-4">(React) Google Books Search</h1>
+              <p className="lead">Search for and save books of interest.</p>
+              <hr className="my-4" />
+              <p className="lead">
+                <Link className="btn btn-default btn-lg" to="/" role="button">New Search</Link>
+                <Link className="btn btn-default btn-lg" to="/saved" role="button">Saved Books</Link>
+              </p>
+            </Header>
+            <Container>
+              <form>
+                <Input
+                  value={this.state.title}
+                  onChange={this.handleInputChange}
+                  name="title"
+                  label="Book Title"
+                  placeholder="Search Book Title (required)"
+                />
+                <FormButton         
+                  onClick={this.handleFormSubmit}
+                  className="btn btn-info"
+                >
+                  Search
+                </FormButton>
+              </form>
+            </Container>
+          </div>
+        );
+    }
 }
